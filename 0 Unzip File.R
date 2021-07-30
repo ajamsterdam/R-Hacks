68742f88e4635c1689de2dc4aaea2eac4769115e
Jamal Jones <aj_jones@yahoo.com>
2021-07-30 14:40:30 -0400

    Lesson 1 Commit

--- 0 Unzip File.R
+++ 0 Unzip File.R
@@ -6,3 +6,38 @@ unzip(zipfile = '1 R Hacks.zip')
 getwd()
 
 list.files(path = '1 R Hacks/', all.files = T)
+
+install.packages('lubridate')
+
+lubridate::date()
+lubridate::today()
+lubridate::month(lubridate::today())
+
+library(lubridate)
+
+library(tidyverse)
+
+install.packages("tidyverse")
+
+
+date()
+
+today()
+
+month(today())
+
+
+
+d <- '2011-12-12'
+
+d2 <- ymd(d)
+
+class(d)
+
+class(d2)
+
+
+Sys.Date()
+
+Sys.sleep(20)
+
